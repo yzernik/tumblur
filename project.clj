682b0d6520872d72864909ed5f677e3f67fcc24c
yzernik <yzernik@gmail.com>
2015-02-14 14:15:46 -0800

    starting to create tumblr oauth workflow

--- project.clj
+++ project.clj
@@ -7,7 +7,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
-                 [environ "0.5.0"]]
+                 [environ "0.5.0"]
+                 [com.cemerick/friend "0.2.0"]
+                 [friend-oauth2 "0.1.1"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
   :hooks [environ.leiningen.hooks]
--- src/tumblure/web.clj
+++ src/tumblure/web.clj
@@ -4,7 +4,54 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
+            [cemerick.friend        :as friend]
+            [friend-oauth2.workflow :as oauth2]
+            [friend-oauth2.util     :refer [format-config-uri]]
             [environ.core :refer [env]]))
+
+(def client-config
+  {:client-id     (env :tumblure-oauth2-client-id)
+   :client-secret (env :tumblure-oauth2-client-secret)
+   :callback      {:domain "http://localhost:5000" ;; replace this for production with the appropriate site URL
+                   :path "/oauth2callback"}})
+
+(defn credential-fn
+  "Upon successful authentication with the third party, Friend calls
+  this function with the user's token. This function is responsible for
+  translating that into a Friend identity map with at least the :identity
+  and :roles keys. How you decide what roles to grant users is up to you;
+  you could e.g. look them up in a database.
+  
+  You can also return nil here if you decide that the token provided
+  is invalid. This could be used to implement e.g. banning users.
+  
+  This example code just automatically assigns anyone who has
+  authenticated with the third party the nominal role of ::user."
+  [token]
+  {:identity token
+   :roles #{::user}})
+
+(def uri-config
+  {:authentication-uri {:url "http://www.tumblr.com/oauth/authorize"
+                        :query {:client_id (:client-id client-config)
+                                :response_type "code"
+                                :redirect_uri (format-config-uri client-config)
+                                :scope "email"}}
+   
+   :access-token-uri {:url "http://www.tumblr.com/oauth/access_token"
+                      :query {:client_id (:client-id client-config)
+                              :client_secret (:client-secret client-config)
+                              :grant_type "authorization_code"
+                              :redirect_uri (format-config-uri client-config)}}})
+
+(def friend-config
+  {:allow-anon? true
+   :workflows   [(oauth2/workflow
+                  {:client-config client-config
+                   :uri-config uri-config
+                   :credential-fn credential-fn})
+                 ;; Optionally add other workflows here...
+                 ]})
 
 (defn splash []
   {:status 200
